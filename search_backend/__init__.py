fea2965d8401de58f35f54cbb9e00f34ab304212
gelkin <mazin.v.a@gmail.com>
2017-10-18 12:14:13 +0200

    Simple boolean search started

--- search_backend/__init__.py
+++ search_backend/__init__.py
@@ -1,3 +1,8 @@
+import os
+
+from nltk import RegexpTokenizer, defaultdict
+
+
 def search(query_string):
     result_list = []
 
@@ -9,3 +14,33 @@ def search(query_string):
         })
 
     return result_list
+
+
+def my_search(query_string):
+    arg_tokens = prepare_arguments(query_string)
+
+
+def prepare_arguments(query_string):
+    tokens = query_string.split()
+
+
+
+
+def get_all_docs(path):
+    docs = sorted([os.path.join(dp, f) for dp, dn, fn in os.walk(path) for f in fn])
+    print(len(docs))
+    print(docs)
+
+
+# todo make index - defaultdict(list)
+# todo do we need to return 'index' or it's just updated by link
+def add_doc_to_index(index: defaultdict, doc: str, doc_id: int, stop_words: set):
+    doc = doc.lower()
+    tokens = doc.split()
+    cnt = 1
+    for token in tokens:
+        if token not in stop_words:
+            index[token] = index[token].append((doc_id, cnt))
+        cnt += 1
+
+
