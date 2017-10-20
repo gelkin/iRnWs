351f024745ebaa24e45af1acdae7727ff39315cd
gelkin <mazin.v.a@gmail.com>
2017-10-20 09:48:24 +0200

    Correction of two letter exchange in a word added

--- two_gram_playground.py
+++ two_gram_playground.py
@@ -1,4 +1,3 @@
-import math
 import re
 
 
@@ -6,13 +5,30 @@ def get_letters_of_alphabet(text):
     return list(sorted({char for char in text}))
 
 
-def get_prob_of_word_in_lang(word, probs):
+def correct_two_letters_exchange(word, letter_probs):
+    candidate_words = [word]
+    for i in range(len(word) - 1):
+        candidate_words.append(word[:i] + word[i + 1] + word[i] + word[i + 2:])
+    words_probs = {}
+    for candidate_word in candidate_words:
+        prob = get_prob_of_word(candidate_word, letter_probs)
+        words_probs[candidate_word] = prob
+    return max(words_probs, key=words_probs.get)
+
+
+def get_prob_of_word(word, letter_probs):
+    """
+    :param word:
+    :param letter_probs: Map of probabilities of one letter to be followed by another one
+    :return: Probability of a 'word' to be build having a probability distribution
+             of one letter to be followed by another one.
+    """
     word = '$' + word + '&'
     prob = 1
     for ch_first, ch_second in zip(word[:-1], word[1:]):
-        if ch_first in probs and ch_second in probs[ch_first]:
+        if ch_first in letter_probs and ch_second in letter_probs[ch_first]:
             # conditional probability
-            prob *= probs[ch_first][ch_second] / sum(probs[ch_first].values())
+            prob *= letter_probs[ch_first][ch_second] / sum(letter_probs[ch_first].values())
         else:
             return 0
     return prob
@@ -45,24 +61,28 @@ def prepare_text(filename, encoding='utf-8'):
         text = text.lower()
         return "".join(["$" + re.sub('\W+', '', word) + "&" for word in text.split()])
 
-ukr_text = prepare_text("./res/simya.txt", encoding='windows-1251')
-rus_text = prepare_text("./res/voina_i_mir.txt", encoding='windows-1251')
 
-ukr_alphabet = get_letters_of_alphabet(ukr_text)
-rus_alphabet = get_letters_of_alphabet(rus_text)
-print(get_letters_of_alphabet(ukr_text))
-print(get_letters_of_alphabet(rus_text))
+def run_example_of_lang_detection(word="як"):
+    ukr_text = prepare_text("./res/simya.txt", encoding='windows-1251')
+    rus_text = prepare_text("./res/voina_i_mir.txt", encoding='windows-1251')
+    ukr_probs = build_probs(ukr_text)
+    rus_probs = build_probs(rus_text)
+    ukr_prob = get_prob_of_word(word, ukr_probs)
+    rus_prob = get_prob_of_word(word, rus_probs)
+    print(ukr_prob)
+    print(rus_prob)
+    if ukr_prob > rus_prob:
+        print("Ukranian")
+    else:
+        print("Russian")
 
-ukr_probs = build_probs(ukr_text)
-rus_probs = build_probs(rus_text)
 
-word = "як"
-ukr_prob = get_prob_of_word_in_lang(word, ukr_probs)
-rus_prob = get_prob_of_word_in_lang(word, rus_probs)
-print(ukr_prob)
-print(rus_prob)
-if ukr_prob > rus_prob:
-    print("Ukranian")
-else:
-    print("Russian")
+def run_example_of_letter_exchange_typos(word='броьш'):
+    rus_text = prepare_text("./res/voina_i_mir.txt", encoding='windows-1251')
+    rus_probs = build_probs(rus_text)
+    best_word = correct_two_letters_exchange(word, rus_probs)
+    print(best_word)
+
+run_example_of_letter_exchange_typos()
+
 
